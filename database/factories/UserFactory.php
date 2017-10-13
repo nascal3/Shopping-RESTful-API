a0409a05b7acc4844377faeb5ea795bcd3dd3f91
nascal3 <nascal3@gmail.com>
2017-10-13 19:04:20 +0300

    seed data to tables ready

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Category;
+use App\Product;
+use App\Seller;
+use App\Transaction;
+use App\User;
 use Faker\Generator as Faker;
 
 /*
@@ -13,7 +18,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     static $password;
 
     return [
@@ -21,5 +26,40 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token'=> $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER])
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['img1.jpg', 'img2.jpg', 'img3.jpg']),
+        'seller_id' => User::all()->random()->id
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id
     ];
 });
